dfcdacc0e2a8760303a0df184207e0822532c214
Grub4K <simonben@abwesend.de>
2018-10-15 17:58:30 +0200

    sample tanks behavior call

--- game_objects/tank.py
+++ game_objects/tank.py
@@ -3,3 +3,10 @@ from engine import *
 
 
 class Tank(InputObject):
+    def on_start(self):
+        self._start_behavior(self.update_function_cr)
+
+    def update_function_cr(self):
+        # Do shit
+        while True:
+            yield
